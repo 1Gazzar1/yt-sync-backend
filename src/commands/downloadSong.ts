2b8e83471422fc372b78c120017bddd4a8dfde7e
1Gazzar1 <mhmdaljzar036@gmail.com>
2025-12-02 16:39:20 +0200

    added download song function that uses yt-dlp

---
+++ src/commands/downloadSong.ts
@@ -0,0 +1,17 @@
+import { exec } from "child_process";
+
+export async function downloadSong(url: string, path: string) {
+    const id = new URLSearchParams(path).get("v");
+
+    const { stdout, stderr } = await exec(
+        `yt-dlp -x --audio-format mp3 
+        --quiet 
+        --add-metadata  --embed-thumbnail 
+        -o "%(title)s.%(ext)s"
+        -P ${path} 
+        ${url}`
+    );
+
+    console.log("stdout:", stdout, "song id:", id);
+    console.error("stderr:", stderr, "song id:", id);
+}
